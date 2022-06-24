382f47bb6ba33f780f510c9df7d4cc4932becc0e
Teslim53 <95858261+Teslim53@users.noreply.github.com>
2022-06-24 15:25:40 +0100

    Add files via upload

---
+++ details.js
@@ -0,0 +1,4 @@
+//Javascript code that prints out my name, height, and country on the screen.
+document.write('Name: Lawal Teslim'+'<br>');
+document.write('Height: 1.7m'+'<br>');
+document.write('Country: Nigeria'+'<br>');
